7e3ddedc5e42155118bb74aea81fedffbc10aebe
Kripan K C <kkc@munichre.com>
2026-02-18 14:36:14 +0100

    Configure deployment to GitHub Pages (Action + next.config)

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",  // <=== Enables static HTML export for GitHub Pages
+    images: {
+        unoptimized: true, // <=== Required for static export (Next.js Image API needs a server)
+    },
+    // If your repo is NOT at the root (e.g. username.github.io/repo-name),
+    // you might need to uncomment and set the repo name here:
+    basePath: "/portfolio-v2",
+};
+
+export default nextConfig;
